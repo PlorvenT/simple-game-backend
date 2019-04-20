65b57a5c79ab60caa270a11801bf92e640a5153c
Roman Bahniuk <bahniuk.rv@gbksoft.com>
2019-04-20 11:28:35 +0300

    Added authentication

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © 2018 GBKSOFT. Web and Mobile Software Development.
+ * See LICENSE.txt for license details.
+ */
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class UsersTableSeeder
+ */
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's clear the users table first
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Let's make sure everyone has the same password and
+        // let's hash it before the loop, or else our seeder
+        // will be too slow.
+        $password = Hash::make('qwerty');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
