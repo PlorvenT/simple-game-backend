f5855978f87f1b0a85557d9556da56079af1c8d5
Roman Bahniuk <bahniuk.rv@gbksoft.com>
2019-04-22 17:30:48 +0300

    task#16: Fixed .gitlab-ci.yml

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,19 @@
-@servers(['web_prod' => 'localhost', 'web_dev' => 'localhost'])
+@setup
+    require __DIR__.'/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::create(__DIR__);
+    try {
+        $dotenv->load();
+    } catch ( Exception $e )  {
+        echo $e->getMessage();
+    }
 
-@task('deploy:prod', ['on' => 'web_prod'])
+    $server = getenv('DEPLOY_SERVER');
+@endsetup
+
+@servers(['web' => $server])
+
+@task('deploy:prod', ['on' => 'web'])
+    echo $server
     php artisan config:cache
     php artisan migrate
 @endtask
